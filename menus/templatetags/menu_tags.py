fc614648faba580a1f447c0e6c7bbedf6e0ae788
Matt Austin <mail@mattaustin.me.uk>
2010-10-24 14:45:28 +0800

    Added 'active' class to active links in menu. Added 'a' element around menu items.

--- menus/templatetags/menu_tags.py
+++ menus/templatetags/menu_tags.py
@@ -1,14 +1,16 @@
 from django import template
 from menus.models import Menu
 
+
 register = template.Library()
 
-@register.inclusion_tag('menus/_menu.html')
-def menu(slug, extra_class=None):
-    print slug
+@register.inclusion_tag('menus/_menu.html', takes_context=True)
+def menu(context, slug, extra_class=None):
     try:
         menu = Menu.objects.active().get(slug=slug)
     except Menu.DoesNotExist:
         menu = None
-    return {'menu': menu, 'extra_class': extra_class}
+    request = context['request']
+    return {'menu': menu, 'extra_class': extra_class,
+        'request': request}
 
