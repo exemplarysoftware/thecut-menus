6e24ef52d53a72a546feb80e1e54cec0b4faf911
Josh Crompton <josh.crompton@gmail.com>
2014-06-06 16:58:07 +0800

    Add tests for menu tag handling lookup by slug or MenuItem instance.

--- thecut/menus/tests/tests.py
+++ thecut/menus/tests/tests.py
@@ -1,10 +1,26 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
-from thecut.menus import templatetags
+from thecut.menus.models import MenuItem
+from thecut.menus.templatetags.menu_tags import menu
+from thecut.menus.tests.factories import MenuItemFactory
 from unittest import TestCase
 
 
 class TestMenuTag(TestCase):
 
-    def test_returns_a_menuitem_list_when_given_a_slug(self):
-        self.assertTrue(True)
+    def setUp(self):
+        self.root = MenuItemFactory(slug='root')
+        self.child = MenuItemFactory(parent=self.root)
+
+
+    def test_returns_a_menuitems_children_when_given_a_slug(self):
+
+        result = menu({}, 'root')
+
+        self.assertIn(self.child, result['menuitem_list'])
+
+    def test_returns_a_menuitems_children_when_given_a_menuitem(self):
+
+        result = menu({}, self.root)
+
+        self.assertIn(self.child, result['menuitem_list'])
