007f339b3b26c1d0e244dab9f32ca04a2d655266
Josh Crompton <josh.crompton@gmail.com>
2014-06-11 16:44:18 +0800

    Only display the descendants of the MenuItem being edited.

--- thecut/menus/static/menus/menus.js
+++ thecut/menus/static/menus/menus.js
@@ -5,18 +5,18 @@ var MenuItemCollection = Backbone.Collection.extend({
 
     model: MenuItem,
 
-    // initialize: function(models, options) {
-    // 	delete this.paginationConfig;
-    // 	this.paginationConfig = {
-    // 	    pretty: false,
-    // 	    ipp: 5,
-    // 	    page_attr: 'page',
-    // 	    ipp_attr: 'limit',
-    // 	    fetchOptions: {'async': false, 'reset': true}
-    // 	};
+    url: function() {
+	// Only list the MenuItems which are descendants of the
+	// MenuItem which is being edited.
+	var base_url = "/admin/menus/menuitem/api/menuitems/";
+	model_id = $("#menuitems").attr("data-root-menuitem-id" || null);
+	console.log('model id', model_id);
+	if (model_id != null) {
+	    return base_url + "?root=" + model_id;
+	};
 
-    url: "/admin/menus/menuitem/api/menuitems/"
-	
+	return base_url;
+    }
 
 });
 
@@ -27,12 +27,9 @@ var MenuItemView = Backbone.View.extend({
     template: _.template($("#menuitem-template").html()),
 
     render: function() {
-	console.log('rendering MenuItemView');
 	var ul = $("#menuitems");
 	var li = $(this.el).html(this.template(this.model.toJSON()));
 	ul.append(li);
-	// console.log('inside menu item view');
-	// console.log(this.model.get('name'));
 	return this;
     },
 });
@@ -43,22 +40,14 @@ var MenuItemsView = Backbone.View.extend({
     tagName: 'ul',
 
     render: function() {
-	console.log('Rendering MenuItemsView');
 	var menuitems = new MenuItemCollection();
-	console.log('Fetching MenuItems');
 	menuitems.fetch({async: false});
-	console.log('Fetched ' + menuitems.length + ' MenuItems:');
 
 	menuitems.forEach(function(menuitem) {
-	    console.log('Creating MenuItemView');
-//	    var el = $("#menuitems");
 	    var itemView = new MenuItemView({model: menuitem});
-	    console.log('rendering...');
 	    itemView.render();
 	    $(this.el).append(itemView.render().el);
 	});
-
-	console.log('Done rendering MenuItemsView');
     }
 });
 
