4c66b4f76d0e388ecd392d42135d7f8841850518
Mark Lockett <mark.lockett@thecut.net.au>
2016-07-28 10:43:32 +0800

    Get unittests to pass in Django 1.8 Refs #8138

--- thecut/menus/tests/factories.py
+++ thecut/menus/tests/factories.py
@@ -2,30 +2,24 @@
 from __future__ import absolute_import, unicode_literals
 from thecut.menus.models import MenuItem
 import factory
-from django import VERSION
-
-if VERSION > (1, 5):
-    from django.contrib.auth import get_user_model
-    user_model = get_user_model()
-else:
-    from django.contrib.auth.models import User
-    user_model = User
+from django.contrib.auth import get_user_model
+user_model = get_user_model()
 
 
 class UserFactory(factory.django.DjangoModelFactory):
-
-    FACTORY_FOR = user_model
-    FACTORY_DJANGO_GET_OR_CREATE = ('username', )
+    class Meta:
+        model = user_model
+        django_get_or_create = ('username', )
 
     username = factory.Sequence(lambda n: 'user_{0}@example.com'.format(n))
 
 
 class MenuItemFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = MenuItem
+        django_get_or_create = ('slug', )
 
-    FACTORY_FOR = MenuItem
-    FACTORY_DJANGO_GET_OR_CREATE = ('slug', )
-
-    name = factory.Sequence(lambda n: 'Menu Item {0}'.format(n))
+    title = factory.Sequence(lambda n: 'Menu Item {0}'.format(n))
     slug = 'default-menu-item'
     created_by = factory.SubFactory(UserFactory)
     updated_by = factory.SubFactory(UserFactory)
