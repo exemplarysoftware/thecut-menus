515dfbd38b80b341a9cfbc2cf30a6d7760b977dd
Mark Lockett <mark.lockett@thecut.net.au>
2016-07-28 13:19:30 +0800

    Add unit tests for view link parameters Refs #8103

--- thecut/menus/tests/tests.py
+++ thecut/menus/tests/tests.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import, unicode_literals
 from thecut.menus.templatetags.menu_tags import menu
 from thecut.menus.tests.factories import MenuItemFactory
 from django.test import TestCase
+from django.test import Client
+from django.core.urlresolvers import reverse
 
 
 class TestMenuTag(TestCase):
@@ -22,3 +24,23 @@ class TestMenuTag(TestCase):
         result = menu({}, self.root)
 
         self.assertIn(self.child, result['menuitem_list'])
+
+
+class TestViewLinkReverse(TestCase):
+    # We test reversing the URL and passing parameter via a reversed URl
+
+    def test_reverse_links_to_the_correct_URL_without_parameters(self):
+        url = reverse('hello:world')
+        self.assertEquals(url, '/hello/world/')
+        response = client.get(url)
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.content, "Hello world")
+
+    def test_reverse_links_to_the_correct_URL_with_parameters(self):
+        url = reverse('hello:world2 1111 2222')
+        self.assertEquals(url, '/hello/world2/1111/2222/')
+        response = client.get(url)
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.content, "Hello world2 1111 2222")
+
+        
